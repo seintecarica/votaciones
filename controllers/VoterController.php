401d1de7bff3d46646e31e71ca7a6b7b6135981b
Rodrigo Quisbert <seintecarica@gmail.com>
2024-01-02 17:15:07 -0300

    Update VoterController.php

--- controllers/VoterController.php
+++ controllers/VoterController.php
@@ -7,7 +7,6 @@ class VoterController{
     public function saveForm($datos){
         // Crear una instancia del modelo
         $formModel = new VoterModel();
-        //echo $datos['options'];
         // Llamar al método del modelo para guardar los datos
         if($formModel->saveForm($datos)){
             $_SESSION['mensaje'] = "Registro guardado exitosamente.";
@@ -15,7 +14,6 @@ class VoterController{
         else{
             $_SESSION['mensaje'] = "Rut ya registrado.";
         }
-        //include('views/form.php');
         header('Location: views/form.php');
     }
 }
